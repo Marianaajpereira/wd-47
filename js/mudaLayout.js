99bd746338a36494f3b1a559e9c4a5535ab5b8b0
Marianaajpereira <marizinh_eminem@hotmail.com>
2017-11-13 17:01:31 -0200

    aula 1 - 13/11

---
+++ js/mudaLayout.js
@@ -0,0 +1,17 @@
+var btn = document.querySelector("#mudaLayout")
+var mural = document.querySelector(".mural")
+
+function mudaTexto() {
+  if (mural.classList.contains("mural--linha")) {
+    btn.textContent = "Linhas"
+  } else {
+    btn.textContent = "Blocos"
+  }
+}
+
+function mudaLayout() {
+  mural.classList.toggle("mural--linha")
+}
+
+btn.addEventListener("click", mudaTexto)
+btn.addEventListener("click", mudaLayout)
